3013c0402b3f7a034aed9f84dac7cd8e9c6c02df
TheEmperor342 <83999665+TheEmperor342@users.noreply.github.com>
2022-11-15 21:37:27 +0530

    feat: `/` route website

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import { readdirSync, readFileSync } from "fs";
 const app: express.Application = express();
 const port: number = Number(process.env.PORT) || 3001;
 
+app.use(express.static("./static"));
+
 app.get("/img", (req: express.Request, res: express.Response) => {
 	res.set("Content-Type", "image/svg+xml");
 	const img: string = req.query.img as string;
